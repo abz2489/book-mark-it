c9e84bbb10222d57189a784b15c0998d9cf36fc3
Abbie <abz2489@gmail.com>
2023-11-30 20:23:58 +0000

    Create book model

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 ## Code
 - Laura Mayock's codu article on Meta Tags.
 [Meta Tags - All your work could be for nothing!](https://www.codu.co/articles/meta-tags-all-your-work-could-be-for-nothing-32p5y-ej)
+
+## Content
+- Books, details and covers
+[Bloomsbury Publishing](https://www.bloomsbury.com/uk/)
--- books/models.py
+++ books/models.py
@@ -10,3 +10,18 @@ class Category(models.Model):
     
     def get_friendly_name(self):
         return self.friendly_name
+
+
+class Book(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    title = models.CharField(max_length=255)
+    summary = models.TextField()
+    author = models.CharField(max_length=255)
+    published = models.DateField(null=True, blank=True)
+    isbn = models.IntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    cover_url = models.URLField(max_length=1024, null=True, blank=True)
+    cover = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
