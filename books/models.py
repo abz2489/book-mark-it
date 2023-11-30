9a1b29f533e0baef6a21344cb434f4a935792bf8
Abbie <abz2489@gmail.com>
2023-11-30 19:32:12 +0000

    Upload fixture files and create category model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    friendly_name = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name
