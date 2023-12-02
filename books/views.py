cefebcaf60e2185174dd6104f9f0c30d3932e60a
Abbie <abz2489@gmail.com>
2023-12-02 10:10:21 +0000

    Add books view urls and template

--- books/views.py
+++ books/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+def all_books(request):
+    """This view displays all books"""
+
+    books = Book.objects.all()
+
+    context = {
+        "books": books,
+    }
+    return render(request, "books/books.html", context)
