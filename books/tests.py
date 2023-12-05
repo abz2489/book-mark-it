c75ae5755fce28080a09d6db0b71e5a7e69b70a1
Abbie <abz2489@gmail.com>
2023-12-05 23:47:03 +0000

    Create tests and test category method in books

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
 
+from books.models import Category
+
 # Create your tests here.
+class TestCategory(TestCase):
+    """Test Category model"""
+    CAT_NAME = "test cat"
+    CAT_FRIENDLY_NAME = "Test Cat"
+
+    def setUp(self):
+        self.category = Category.objects.create(
+            name=self.CAT_NAME,
+            friendly_name=self.CAT_FRIENDLY_NAME
+        )
+    
+    def test_instance(self):
+        """Test the category instance has been created"""
+        self.assertEqual(self.category.name, self.CAT_NAME)
+        self.assertEqual(self.category.friendly_name, self.CAT_FRIENDLY_NAME)
+
+    def test_str(self):
+        """Test string method"""
+        self.assertEqual(str(self.category), self.CAT_NAME)
+
+    def test_get_friendly_name(self):
+        """Test get friendly name method"""
+        cat = Category.objects.get(name=self.CAT_NAME)
+        friendly_name = cat.get_friendly_name()
+        self.assertEqual(friendly_name, self.CAT_FRIENDLY_NAME)
