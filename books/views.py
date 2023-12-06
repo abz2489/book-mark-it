ca12ca6af1eb5c3fbeba3847f2b57de4c63c2196
Abbie <abz2489@gmail.com>
2023-12-06 23:02:28 +0000

    Add book search functionality

--- books/views.py
+++ books/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import get_object_or_404, render
+from unicodedata import category
+from django.shortcuts import get_object_or_404, render, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Book
 
 # Create your views here.
@@ -6,9 +9,22 @@ def all_books(request):
     """This view displays all books"""
 
     books = Book.objects.all()
+    query = None
+
+    if request.GET:
+        if "q" in request.GET:
+            query = request.GET["q"]
+            print(f"Query: {query}")
+            if not query:
+                messages.error(request, "No search criteria entered!")
+                return redirect(reverse('books'))
+
+            queries = Q(title__icontains=query) | Q(category__name__icontains=query) | Q(author__icontains=query)
+            books = books.filter(queries)
 
     context = {
         "books": books,
+        "search_term": query
     }
     return render(request, "books/books.html", context)
 
@@ -16,9 +32,9 @@ def all_books(request):
 def book_summary(request, book_id):
     """This view displays an individual summary of a selected book"""
 
-    book = get_object_or_404(Book, id=book_id)
+    books = get_object_or_404(Book, id=book_id)
 
     context = {
-        "book": book,
+        "books": books,
     }
     return render(request, "books/book_summary.html", context)
