f9122f74db70e33fae7ed9607ffb1c9923f29bef
Abbie <abz2489@gmail.com>
2023-12-06 12:52:41 +0000

    Write and test book model

--- books/tests.py
+++ books/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from books.models import Category
+from books.models import Category, Book
 
 # Create your tests here.
 class TestCategory(TestCase):
@@ -17,10 +17,9 @@ class TestCategory(TestCase):
     def test_instance(self):
         """Test the category instance has been created"""
         self.assertEqual(self.category.name, self.CAT_NAME)
-        self.assertEqual(self.category.friendly_name, self.CAT_FRIENDLY_NAME)
 
     def test_str(self):
-        """Test string method"""
+        """Test string method for Category"""
         self.assertEqual(str(self.category), self.CAT_NAME)
 
     def test_get_friendly_name(self):
@@ -28,3 +27,28 @@ class TestCategory(TestCase):
         cat = Category.objects.get(name=self.CAT_NAME)
         friendly_name = cat.get_friendly_name()
         self.assertEqual(friendly_name, self.CAT_FRIENDLY_NAME)
+
+
+class TestBook(TestCase):
+    BOOK_TITLE = "Test Title"
+    BOOK_SUMMARY = "This is a test summary for {BOOK_TITLE}"
+    BOOK_AUTHOR = "Test author"
+    BOOK_ISBN = 123456789
+    BOOK_PRICE = 5.99
+
+    def setUp(self):
+        self.book = Book.objects.create(
+            title=self.BOOK_TITLE,
+            summary=self.BOOK_SUMMARY,
+            author=self.BOOK_AUTHOR,
+            isbn=self.BOOK_ISBN,
+            price=self.BOOK_PRICE
+        )
+    
+    def test_instance(self):
+        """Test the book instance has been created with minimum data"""
+        self.assertEqual(self.book.title, self.BOOK_TITLE)
+    
+    def test_str(self):
+        """Test string method for Book"""
+        self.assertEqual(str(self.book), self.BOOK_TITLE)
