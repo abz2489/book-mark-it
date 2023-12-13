9a601350283d7ca72d1fcb93d68d820feb1cb67b
Abbie <abz2489@gmail.com>
2023-12-13 20:22:23 +0000

    Add functionality for adding books to basket

--- basket/urls.py
+++ basket/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.view_basket, name='basket'),
+    path('add/<book_id>/', views.add_to_basket, name='add_to_basket'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def view_basket(request):
     """A view that renders basket items page"""
 
     return render(request, "basket/basket.html")
+
+
+def add_to_basket(request, book_id):
+    """Add book quantity to basket"""
+
+    quantity = int(request.POST.get("quantity"))
+    redirect_url = request.POST.get("redirect_url")
+    basket = request.session.get('basket', {})
+
+    if book_id in list(basket.keys()):
+        basket[book_id] += quantity
+    else:
+        basket[book_id] = quantity
+
+    request.session["basket"] = basket
+    print(request.session["basket"])
+    return redirect(redirect_url)
