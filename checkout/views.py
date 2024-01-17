773814e7545e519e54aa54c9e2f847a1d9f91d3d
Abbie <abz2489@gmail.com>
2024-01-17 22:40:00 +0000

    Update checkout view with prepopulated user info

--- checkout/views.py
+++ checkout/views.py
@@ -49,6 +49,7 @@ def checkout(request):
             "street_address2": request.POST.get("street_address2", ""),
             "county": request.POST.get("county", ""),
         }
+
         order_form = OrderForm(form_data)
         if order_form.is_valid():
             order = order_form.save(commit=False)
@@ -96,7 +97,24 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY
         )
 
-        order_form = OrderForm()
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    "full_name": profile.user.get_full_name(),
+                    "email": profile.user.email,
+                    "phone_number": profile.default_phone_number,
+                    "street_address1": profile.default_street_address1,
+                    "street_address2": profile.default_street_address2,
+                    "town_or_city": profile.default_town_or_city,
+                    "county": profile.default_county,
+                    "postcode": profile.default_postcode,
+                    "country": profile.default_country
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, "Stripe public key is missing. \
